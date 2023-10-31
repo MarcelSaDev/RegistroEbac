452afb01bd4ede384f48f12c992605bffc483887
MarcelSaDev <149501539+MarcelSaDev@users.noreply.github.com>
2023-10-31 10:50:19 -0300

    Opção de saída
    Incluso no sistema a opção de saída para o usuário

--- Registro.c
+++ Registro.c
@@ -129,6 +129,7 @@ int main()
 		printf("\t1 - Registrar Nomes\n");
 		printf("\t2 - Consultar Nomes\n");
 		printf("\t3 - Excluir Nomes\n\n"); 
+		printf("\t4 - Sair do Sistema\n\n");
 		printf("Opção: ");//fim do menu
 	
 		scanf("%d",&opcao); //armazenando escolha dos usuário
@@ -147,6 +148,11 @@ int main()
 		
 			case 3:
 			deletar();
+			break;
+			
+			case 4:
+			printf("Obrigado por utilizar o sistema!\n");
+			return 0;
 			break;
 					
 			default:
